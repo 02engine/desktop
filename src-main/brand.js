5764ea8cba42348ab68170a19c7e6b76a19817e8
DDguan <94268952+DDguan2010@users.noreply.github.com>
2025-08-09 13:47:56 +0800

    first commit

---
+++ src-main/brand.js
@@ -0,0 +1,9 @@
+const PLATFORM_NAME = '02Engine';
+const APP_NAME = '02Engine Desktop';
+const PACKAGER_NAME = 'TurboWarp Packager';
+
+module.exports = {
+  PLATFORM_NAME,
+  APP_NAME,
+  PACKAGER_NAME
+};
